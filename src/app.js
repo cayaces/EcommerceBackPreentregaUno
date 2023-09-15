5c1a806dc63cfb08c53cef60d23364790cfc435a
cayaces <c.ayaces@gmail.com>
2023-09-15 04:00:36 -0300

    Commit listo

--- src/app.js
+++ src/app.js
@@ -2,17 +2,15 @@ const express = require('express');
 const path = require('path');
 const productsRouter = require('./routes/products.router');
 const cartsRouter = require('./routes/carts.router');
+
 const app = express();
 const PORT = 8080;
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-
 app.use('/', productsRouter);
 app.use('/', cartsRouter);
-
-
 
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en portal ${PORT}`);
--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-// Array de productos (simulando una base de datos)
 const products = [
     {
         nombre: "snapy",
         precio: 25000,
         stock: 50,
         categoria: "juguetes",
-        imagen: "imagen/imagen",
+        codigo: "abc1",
         id: 1
     },
     {
@@ -16,55 +15,51 @@ const products = [
         precio: 1000,
         stock: 30,
         categoria: "juguetes",
-        imagen: "imagen/imagen",
-        id: 4
+        codigo: "abc2",
+        id: 2
     },
     {
         nombre: "lubricante",
         precio: 10000,
         stock: 10,
         categoria: "cosmeticos",
-        imagen: "imagen/imagen",
-        id: 2
+        codigo: "abc3",
+        id: 3
     },
     {
         nombre: "aceite de masaje",
         precio: 5000,
         stock: 40,
         categoria: "cosmeticos",
-        imagen: "imagen/imagen",
-        id: 5
+        codigo: "abc4",
+        id: 4
     },
     {
         nombre: "baby doll",
         precio: 25000,
         stock: 20,
         categoria: "lenceria",
-        imagen: "imagen/imagen",
-        id: 3
+        codigo: "abc5",
+        id: 5
     },
     {
         nombre: "transparente negro",
         precio: 35000,
         stock: 50,
         categoria: "lenceria",
-        imagen: "imagen/imagen",
+        codigo: "abc6",
         id: 6
     }
 ]
 
-
-// Ruta para obtener todos los productos
 router.get('/api/products', (req, res) => {
     res.json({ products });
 });
 
-// Ruta para obtener un producto específico
 router.get('/api/products/:pid', (req, res) => {
     const pid = parseInt(req.params.pid);
     console.log(pid)
 
-    // Ejemplo de búsqueda en un array de productos (reemplaza esta lógica con tu base de datos)
     const product = products.find((product) => product.id === pid);
 
     if (!product) {
@@ -74,42 +69,39 @@ router.get('/api/products/:pid', (req, res) => {
     return res.json(product);
 });
 
-// Ruta para agregar un nuevo producto
 router.post('/api/products', (req, res) => {
 
     const newProduct = req.body;
 
-    // Validamos que se proporcionen todos los campos
     if (!newProduct.id ||
-        !newProduct.name ||
-        !newProduct.price ||
-        !newProduct.description ||
-        !newProduct.code ||
+        !newProduct.nombre ||
+        !newProduct.precio ||
+        !newProduct.descripcion ||
+        !newProduct.codigo ||
         !newProduct.stock ||
-        !newProduct.category) {
-        return res.status(400).json({ error: 'Debe proporcionar todos los campos (id, name, price, description, code, stock, category).' });
+        !newProduct.categoria) {
+        res.json({ mesagge: "Producto agregado" })
+    } else {
+
+        return res.status(400).json({ message: 'Debe llenar todos los campos' });
     }
 
     products.push(newProduct);
 
-    res.json({ message: 'Producto agregado correctamente.' });
-
 });
 
-// Ruta para actualizar un producto por su ID (PUT /:pid)
 router.put('/api/products/:pid', (req, res) => {
     const pid = parseInt(req.params.pid);
     const updateFields = req.body;
 
-    // Validamos que se proporcionen campos para actualizar
     if (Object.keys(updateFields).length === 0) {
-        return res.status(400).json({ error: 'Debe proporcionar al menos un campo para actualizar.' });
+        return res.status(400).json({ message: 'Tienes que completar al menos un campo' });
     }
 
     const productIndex = products.findIndex((product) => product.id === pid);
 
     if (productIndex === -1) {
-        return res.status(404).json({ error: 'Producto no encontrado.' });
+        return res.status(404).json({ message: 'Producto no encontrado.' });
     }
 
     products[productIndex] = {
@@ -120,10 +112,9 @@ router.put('/api/products/:pid', (req, res) => {
     return res.json(products[productIndex]);
 });
 
-// Ruta para eliminar un producto por su ID (DELETE /:pid)
-router.delete('/api/products/:pid', (req, res) => {
-    const pid = parseInt(req.params.pid);
 
+router.delete('/api/products/:pid', (req, res) => {
+    const pid = parseInt(req.params.pid); 
 
     const productIndex = products.find((product) => product.id === pid);
 
